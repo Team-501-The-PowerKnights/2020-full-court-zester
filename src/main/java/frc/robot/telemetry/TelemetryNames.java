ad9a452f75029f194d44420428f2ad3f9a220813
Cal-Hagner <caleb.hagner@pm.me>
2020-01-18 17:57:34 -0500

    Add licensing to telemetry

--- src/main/java/frc/robot/telemetry/TelemetryNames.java
+++ src/main/java/frc/robot/telemetry/TelemetryNames.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 Team 501 - The PowerKnights. All Rights Reserved.       */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the 2020 Team 501 - The PowerKnights BSD license    */
+/* file in the root directory of the project.                                 */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot.telemetry;
 
 public final class TelemetryNames {
