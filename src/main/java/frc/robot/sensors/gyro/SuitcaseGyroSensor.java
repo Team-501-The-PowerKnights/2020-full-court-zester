6c84044db22268bdf6f67ad9a999ae3cd7c33238
Cal-Hagner <caleb.hagner@pm.me>
2020-01-29 19:02:55 -0500

    Update licensing

--- src/main/java/frc/robot/sensors/gyro/SuitcaseGyroSensor.java
+++ src/main/java/frc/robot/sensors/gyro/SuitcaseGyroSensor.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2020 Team 501 - The PowerKnights. All Rights Reserved.       */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* must be accompanied by the 2020 Team 501 - The PowerKnights BSD license    */
+/* file in the root directory of the project.                                 */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.sensors.gyro;
@@ -10,7 +10,6 @@ package frc.robot.sensors.gyro;
 import org.slf4j.Logger;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.telemetry.TelemetryNames;
 import riolog.RioLogger;
 
