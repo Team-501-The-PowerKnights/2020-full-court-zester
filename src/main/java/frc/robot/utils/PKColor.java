b5d72d09a4dcaade8556dd81bec9dd665c7be472
Java4First <47263257+Java4First@users.noreply.github.com>
2020-01-31 19:13:27 -0500

    Tweak pkcolor for name (#27)
    * Return to Stub for Drive
    
    * Finish Implementation

--- src/main/java/frc/robot/utils/PKColor.java
+++ src/main/java/frc/robot/utils/PKColor.java
@@ -10,19 +10,33 @@ package frc.robot.utils;
 import edu.wpi.first.wpilibj.util.Color;
 
 /**
- * Add your docs here.
+ * This class extends the standard <code>WPILib</code> implementation to add a
+ * string for the name. This provides a quicker 'lookup' when doing telemetry
+ * reporting or logging.
+ * 
+ * The RGB values used to create the colors are from RevRobotics and are tuned
+ * to their sensor and the hues for the color wheel used in this years game.
  */
 public class PKColor extends Color {
 
-    public PKColor(double red, double green, double blue) {
+    public final String name;
+
+    public PKColor(double red, double green, double blue, String name) {
         super(red, green, blue);
+        this.name = name;
     }
 
-    public static final Color blueTarget = new PKColor(0.143, 0.427, 0.429);
+    public static final PKColor blueTarget = new PKColor(0.143, 0.427, 0.429, "blue");
 
-    public static final Color greenTarget = new PKColor(0.197, 0.561, 0.240);
+    public static final PKColor greenTarget = new PKColor(0.197, 0.561, 0.240, "green");
 
-    public static final Color yellowTarget = new PKColor(0.361, 0.524, 0.113);
+    public static final PKColor yellowTarget = new PKColor(0.361, 0.524, 0.113, "yellow");
 
-    public static final Color redTarget = new PKColor(0.561, 0.232, 0.114);
+    public static final PKColor redTarget = new PKColor(0.561, 0.232, 0.114, "red");
+
+    /**
+     * A constant to use for invalid colors (in case a match solution isn't found).
+     */
+    public static final PKColor invalidTarget = new PKColor(0, 0, 0, "invalid");
+
 }
