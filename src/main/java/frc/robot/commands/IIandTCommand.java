7e29d1acbe8f9d41935054b997847ff955669cca
Java4First <47263257+Java4First@users.noreply.github.com>
2020-02-09 12:59:51 -0500

    Stub iandt implementation (#60)
    * Remove Robot Personality from CodeVersion
    
    * Create a Marker Interface

---
+++ src/main/java/frc/robot/commands/IIandTCommand.java
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import java.io.Serializable;
+
+/**
+ * Defines a marker interface for commands which are meant to function for use
+ * in I&T of the robot.
+ */
+public interface IIandTCommand extends Serializable {
+}
