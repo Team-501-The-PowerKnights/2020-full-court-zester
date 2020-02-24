e99d7caafaf42a7798253cc148b1615128d2da0b
Cal-Hagner <caleb.hagner@pm.me>
2020-02-24 11:01:53 -0500

    License Header Consistency

--- src/main/java/frc/robot/commands/turret/TurretJogCWCommand.java
+++ src/main/java/frc/robot/commands/turret/TurretJogCWCommand.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2020 Team 501 - The PowerKnights. All Rights Reserved.       */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* must be accompanied by the 2020 Team 501 - The PowerKnights BSD license    */
+/* file in the root directory of the project.                                 */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.commands.turret;
