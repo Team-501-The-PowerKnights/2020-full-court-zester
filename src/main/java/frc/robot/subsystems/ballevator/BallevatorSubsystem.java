16729374153af1b16da2a9e79db6aa31c3e396b2
java4first <java4first@thelewins.net>
2020-02-28 10:36:32 -0500

    Set Brake and Factory Reset on Construct

--- src/main/java/frc/robot/subsystems/ballevator/BallevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ballevator/BallevatorSubsystem.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems.ballevator;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import org.slf4j.Logger;
@@ -35,6 +36,8 @@ class BallevatorSubsystem extends BaseBallevatorSubsystem {
         logger.info("constructing");
 
         motor = new TalonSRX(51);
+        motor.configFactoryDefault();
+        motor.setNeutralMode(NeutralMode.Brake);
 
         limit = new DigitalInput(9);
 
